282e683b2459a49069d64f1088f3e928f9b8763a
swaraj bachu <rajswaraj.r@gmail.com>
2023-04-08 21:34:34 +0530

    🎨 style(globals.css): add global styles for body ✨ feat(index.tsx): add CreatePostWizard component and display posts with author information 🐛 fix(posts.ts): limit getAll query to 100 posts and add author information to the response The global styles for the body have been added to improve the overall look and feel of the application. The CreatePostWizard component has been added to allow users to create new posts. The posts are now displayed with the author's information. The getAll query has been limited to 100 posts and author information has been added to the response.

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,36 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "(~/)/server/api/trpc";
+import { clerkClient } from "@clerk/nextjs/server";
+import { User } from "@clerk/nextjs/dist/api";
+
+const filterUserForClient = (user:User) => {
+
+    return{
+        id: user.id, name: user.username, profilePicture: user.profileImageUrl
+    }
+
+}
 
 export const postRouter = createTRPCRouter({
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({  ctx }) => {
+    const post = await  ctx.prisma.post.findMany(
+        {
+            take: 100,
+        }
+    );
+    const users = (
+        await clerkClient.users.getUserList(
+        {
+            userId: post.map((p) => p.autherId),
+            limit:100
+        }
+    )).map(filterUserForClient);
+    console.log(users)
+    return post.map((post) => ({
+        post,
+        auther: users.find((user) => user.id === post.autherId),
+    }))
   }),
 });
